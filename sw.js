90899310215d9d88cf7913d70d06faa47ea34176
we1kin <2635127929@qq.com>
2024-08-08 09:32:33 +0800

    Site updated: 2024-08-08 09:32:33

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723080393693";
+const VERSION = "1723080749896";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
